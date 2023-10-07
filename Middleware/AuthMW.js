6a3e4a99fe5ae95037820b952ddc5d50b00ff0a1
Donner-von-Swabia <severett21@ivytech.edu>
2023-10-07 14:59:24 -0400

    Teacher and Student use diffrent Auths

--- Middleware/AuthMW.js
+++ Middleware/AuthMW.js
@@ -1,7 +1,36 @@
 const jwt = require('jsonwebtoken');
 
-const requireAuth = (req, res, next) =>{
+const TeacherAuth = (req, res, next) =>{
+    const IsTeacher = req.cookies.Teacher;
     const token = req.cookies.jwt;
+    
+    if (token){
+        jwt.verify(token,"Helios", (err, decodedToken) =>{
+            if (err){
+                console.log(err.message);
+                res.redirect('/login');
+            }
+            else{
+                if (IsTeacher){
+                    console.log(decodedToken);
+                    next();
+                }
+                else{
+                    console.log('Not a teacher')
+                    res.redirect('/login');
+                }
+                
+            }
+        });
+    }
+    else{
+        res.render('login',{title:'Login',siteName:'A Class Coding'});
+    };
+};
+
+const StudentAuth = (req, res, next) =>{
+    const token = req.cookies.jwt;
+    
     if (token){
         jwt.verify(token,"Helios", (err, decodedToken) =>{
             if (err){
@@ -11,12 +40,9 @@ const requireAuth = (req, res, next) =>{
             else{
                 console.log(decodedToken);
                 next();
-            }
-        });
-    }
+            }});}
     else{
         res.render('login',{title:'Login',siteName:'A Class Coding'});
-    }
-}
-
-module.exports = {requireAuth}
+    };
+};
+module.exports = {TeacherAuth,StudentAuth}
--- Routes/courseRoutes.js
+++ Routes/courseRoutes.js
@@ -1,22 +1,23 @@
 const { Router } = require('express');
 const router = Router();
 const courseController = require('../Controller/CourseController')
+const {TeacherAuth,StudentAuth} = require('../Middleware/AuthMW')
 
 //Create Course
-router.get('/create_course', courseController.course_create_get)
-router.post('/create_course', courseController.course_create_post)
+router.get('/create_course',TeacherAuth, courseController.course_create_get)
+router.post('/create_course',TeacherAuth, courseController.course_create_post)
 
 //Update Course
-router.get('/updateCourse/:id', courseController.updateCourse_get)
-router.post('/updateCourse/:id', courseController.updateCourse_post)
+router.get('/updateCourse/:id',TeacherAuth, courseController.updateCourse_get)
+router.post('/updateCourse/:id',TeacherAuth, courseController.updateCourse_post)
 
 //Teacher Page
-router.get('/teacher', courseController.teacher_get)
-router.get('/teacher/:id', courseController.teacher_details_get)
-router.delete('/teacher/:id', courseController.teacher_details_delete)
+router.get('/teacher',TeacherAuth, courseController.teacher_get)
+router.get('/teacher/:id',TeacherAuth, courseController.teacher_details_get)
+router.delete('/teacher/:id',TeacherAuth, courseController.teacher_details_delete)
 
 //Student Page
-router.get('/student',courseController.student_get)
+router.get('/student',StudentAuth,courseController.student_get)
 
 
 module.exports = router;
